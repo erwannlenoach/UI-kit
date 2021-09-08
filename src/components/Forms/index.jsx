20cc928933dc5fc144ac72e26ec26b0380ddc7a0
Erwann Le Noac'h <erwann.lenoach@hotmail.fr>
2021-09-08 15:18:44 +0200

    basis form

---
+++ src/components/Forms/index.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import './style.scss'
+
+const Forms = () => {
+
+    const RegexName = `^[A-Za-z( ')]*$`
+
+    const RegexEmail = '(^[a-zA-Z0-9_.-]*)([@])([a-z]+)(.)([a-z]+)'
+
+
+    return (
+
+        <div id='container'>
+            <form action="" method="get" class="form">
+                <div class="form">
+                    <label for="name">First name</label>
+                    <input type="text" id="name" placeholder="Your first name ... " pattern={RegexName} minlength="3" required />
+                </div>
+
+                <div class="form">
+                    <label for="lastname">Last name</label>
+                    <input type="text" id="lastname" placeholder="Your last name ... " pattern={RegexName} minlength="3" required />
+                </div>
+
+                <div class="form">
+                    <label for="age">Age</label>
+                    <input type="number" id="age-input" placeholder="Your age ... " min="18" required />
+                </div>
+
+                <div class="form" id="email">
+                    <label for="form">Email</label>
+                    <input type="text" id="email-input" placeholder="Your email ..." pattern={RegexEmail} required />
+                </div>
+
+                <div class="form">
+                    <label for="email">Confirmation of the email</label>
+                    <input type="text" id="confirmed-email-input" placeholder="Please confirm your email ... " onblur="confirmEmail()" pattern={RegexEmail} required />
+                </div>
+
+                <div id="password-div">
+                    <div class="form" id="password">
+                        <label for="password" >Password</label>
+                        <input type="password" id="password-input" placeholder="please choose a password with at least 6 characters ... " onblur="confirmEmail()" required />
+                    </div>
+
+                    <div class="form" id="confirmation-password">
+                        <label for="password">Confirmation of the password</label>
+                        <input type="password" id="confirmed-password-input" placeholder="Please confirm your password ... " onblur="confirmPassword()" required />
+                    </div>
+                </div>
+    
+                <div id="CGU">
+                    <input type="radio" id="CGU-radio" required />
+                    <label for="name">I hereby agree to the Terms of Use </label>
+                </div>
+
+                <div id="send">
+                    <button>Envoyer</button>
+
+                </div>
+
+            </form>
+        </div>
+
+
+    )
+
+}
+
+export default Forms
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Forms from './components/Forms'
+
+
+const App = () => {
+  return <>
+    <Forms />
+  </>
+
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
