fedfb4e37cb5878bd891c7c201d36b5610387f0b
Erwann Le Noac'h <erwann.lenoach@hotmail.fr>
2021-09-09 16:41:35 +0200

    navbar basis

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './style.scss'
+
+const Navbar = () => {
+
+    const content = 'Button'
+
+    return (
+
+        <div id='container'>
+            <h2>Navbar</h2>
+            <nav id="navbar">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+                <div id="search-navbar">
+                    <input type="text" id="search-navbar-input" placeholder="Your search" />
+                </div>
+                <button class="button" id="button-plain-blue">Download</button>
+            </nav>
+        </div>
+
+
+    )
+
+}
+
+export default Navbar
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Forms from './components/Forms'
 import Buttons from 'components/Buttons'
+import Navbar from 'components/Navbar';
 
 
 const App = () => {
   return <>
+    <Navbar/>
     <Buttons/>
     <Forms />
   </>
